98b2fa9cf8559504a89336441b517c9fef0a2d47
ismail <ismailkoormath037@gmail.com>
2024-01-17 14:18:29 +0530

    footer completed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,12 +8,12 @@ const Footer = () => {
     return (
         <div className={classes.Footer}>
             <div className={classes.logo}><img src={logo} alt="logo" /><p>we believe in providing our clients with personalized and efficient logistics solutions that meet their unique needs.</p></div>
-            <div><h2>Navigation</h2><ul><li>Home</li>
+            <div className={classes.navigations}><h2 className={classes.headings}>Navigation</h2><ul><li>Home</li>
                 <li>About Us</li>
                 <li>Services</li>
                 <li>Contact Us</li></ul></div>
-            <div><h2>Recent Project</h2><img src={RecentProject} alt="recent-project-img" /></div>
-            <div className={classes.subscribe_portion}><h2>Subscribe Us</h2><input type="text" /><CustomButton>Subscribe Now</CustomButton></div>
+            <div className={classes.recent_project}><h2 className={classes.headings}>Recent Project</h2><img src={RecentProject} alt="recent-project-img" /></div>
+            <div className={classes.subscribe_portion}><h2 className={classes.headings}>Subscribe Us</h2><input type="text" placeholder='Enter Your Email' /><CustomButton>Subscribe Now</CustomButton></div>
         </div>
     )
 }
